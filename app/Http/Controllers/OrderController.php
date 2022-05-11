9a9e9ccc75be8ee8f8487dd03f2bbab8df4ecc6a
ShestakovPD <11pasha@mail.ru>
2022-05-11 11:45:03 +0500

    + SMTP уведомление на почту о заказе

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,12 +7,17 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use Swift_SmtpTransport;
+use Swift_Mailer;
+use Swift_Message;
+
 
 class OrderController extends Controller
 {
     public $product;
     public $order;
     public $orders;
+    public $transport ;
 
     public function getAllOrder(){
 
@@ -59,11 +64,41 @@ class OrderController extends Controller
             foreach ($val as $w)
                 $sum += $w;
 
+            $this->sendMessage($order);
         return view('layouts.site', [
             'orders'=>$orders,
             'sum'=>$sum,
         ]);
 
     }
+
+    public function sendMessage(){
+
+        try {
+        $transport = (new Swift_SmtpTransport('smtp.yandex.ru', 465, 'ssl'))
+            ->setUsername('SPDyamail')
+            ->setPassword('pwbvlvodmuztlkez');
+
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+        // Create a message
+        $message = (new Swift_Message('Wonderful Subject'))
+            ->setFrom(['SPDyamail@yandex.ru' => 'SPDyamail@yandex.ru'])
+            ->setTo(['SPDyamail@yandex.ru'])
+            ->setBody('Пользователь сделал заказ')
+            /*->attach(Swift_Attachment::fromPath('test.php'))*/
+        ;
+            return $mailer->send($message);
+     /*   var_dump(['res' => $result]);*/
+    } catch (Exception $e) {
+         var_dump($e->getMessage());
+         echo '<pre>' . print_r($e->getTrace(), 1);
+          }
+
+    }
+
+
+
 }
 
